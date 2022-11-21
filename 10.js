b09d2851527585f1d8bbc61389465f10ad56a856
Valeryia Visheuskaya <116907996+Visheuskaya@users.noreply.github.com>
2022-11-21 21:58:53 +0100

    Delete 10.js

--- 10.js
+++
@@ -1,7 +0,0 @@
-function hero(bullets, dragons){
-    const bulletsNeeded = dragons * 2;
-     if (bullets >= bulletsNeeded){
-       return true;}
-     else{
-       return false;};//Get Coding!
-   }
