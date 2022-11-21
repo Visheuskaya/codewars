faf89d56eb1287d5153aa704eb50676bf93c4c3d
Valeryia Visheuskaya <116907996+Visheuskaya@users.noreply.github.com>
2022-11-21 21:59:56 +0100

    codewars

---
+++ 12.js
@@ -0,0 +1,9 @@
+function findShort(s){
+    let res = s.split(' ');
+      let a = Infinity;
+      
+      for (let i = 0; i < res.length; i++) {
+        a = Math.min(res[i].length, a);
+      }
+      return a;
+    }
